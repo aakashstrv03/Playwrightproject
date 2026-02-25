31ce69745e975bb8c193b61e6f8c3fcc4b444ed1
aakashstrv03 <aakashstrv03@gmail.com>
2026-02-25 19:17:35 +0530

    Add file

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -4,3 +4,7 @@ test('has title', async ({ page }) => {
   await page.goto('https://www.flipkart.com/'); 
 
 });
+test('has title', async ({ page }) => {
+  await page.goto('https://www.amazon.com/'); 
+
+});
